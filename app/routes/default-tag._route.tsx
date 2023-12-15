fce0012095545b5cd85c62496b8c660bdfe7b6e7
Try Ajitiono <ballinst@gmail.com>
2023-12-15 10:36:21 +0000

    tweak

--- app/routes/default-tag._route.tsx
+++ app/routes/default-tag._route.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
-import { Tag } from "antd";
+import { Badge, Tag } from "antd";
+import { useEffect, useState } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,20 +10,28 @@ export const meta: MetaFunction = () => {
 };
 
 export default function DefaultTAg() {
+  const [isAddedSomething, setIsAddedSomething] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsAddedSomething(true);
+    }, 1000);
+  }, []);
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
       <ul>
         <li>
-          <a href="/">
-            Go to other page with all kind of tag colors
-          </a>
+          <a href="/">Go to other page with all kind of tag colors</a>
         </li>
       </ul>
 
       <div>
         <Tag>Hello</Tag>
       </div>
+
+      <div>{isAddedSomething && <Badge status="success" text="Success" />}</div>
     </div>
   );
 }
